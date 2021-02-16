aa06ad5ddd4aefb96f2ebbd3cac64b3404b3d37a
abhivij <abhishek.kcvijayan@gmail.com>
2021-02-16 23:22:24 +1100

    psd function code cleanup

--- R/psd.R
+++ R/psd.R
@@ -2,32 +2,24 @@
 #'
 #' Compute the Power Spectral Density (psd) transformation of the given scRNA count data
 #'
-#' @param data scRNA count data - cells Vs genes
+#' @param data scRNA count data - cells Vs genes (dim : c x g)
 #' @return psd transformation of input data
 #' @export
 psd <- function(data){
-  data.cor <- cor(data)               #dim : g x g
-  data.psd <- data %*% data.cor       #dim : c x g
-                #compute discrete fourier transform of each cell sample
-  data.psd <- abs(t(
-                  mvfft(t(abs(data.psd)))
-                  )) / dim(data)[2]
-
-  data.psd <- data.psd / rowSums(data.psd)            #subtract rowSums from each column
+  data.psd <- cor(data)                               #dim : g x g
+  data.psd <- data %*% data.psd                       #dim : c x g
+  data.psd <- abs(t(                                  #compute discrete fourier transform
+                    mvfft(t(abs(data.psd)))                         #of each cell sample
+                   )
+                 ) / dim(data)[2]
+  data.psd <- data.psd / rowSums(data.psd)            #divide rowSums from each element
   data.psd <- data.psd * log2(data.psd)
-  data.psd <- sweep(data.psd, 2, colMeans(data.psd))  #subtract colMeans from each row
-
-  # below line doesn't give same results as in psd.py ! Why ?
-  # data.psd <- apply(data.psd, 1, min_max_normalize)
-
-  col_min <- apply(data.psd, 2, min)
-  col_max <- apply(data.psd, 2, max)
-
-  data.psd <- sweep(sweep(data.psd, 2, col_min), 2, col_max - col_min, "/")
-
+  data.psd <- apply(data.psd, 2,
+                    function(x){return (x - mean(x))}
+                    )                                 #subtract column mean from each element
+  data.psd <- apply(data.psd, 2,
+                    function(x){
+                      return ( (x-min(x)) / (max(x)-min(x)) )
+                    })
   return (data.psd)
 }
-
-# min_max_normalize <- function(x){
-#   return (x - min(x)) / (max(x) - min(x))
-# }
