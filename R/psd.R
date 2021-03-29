b1e18496fa16359b27bb3d641715b25ef141f34c
abhivij <abhishek.kcvijayan@gmail.com>
2021-03-29 18:29:14 +1100

    transposed data input, optimizing number of transpose in psd

--- R/psd.R
+++ R/psd.R
@@ -2,24 +2,53 @@
 #'
 #' Compute the Power Spectral Density (psd) transformation of the given scRNA count data
 #'
-#' @param data scRNA count data - cells Vs genes (dim : c x g)
-#' @return psd transformation of input data
+#' @param data scRNA count data - genes Vs cells (dim : g x c)
+#' @return psd transformation of input data (dim : g x c)
 #' @export
 psd <- function(data){
-  data.psd <- cor(data)                               #dim : g x g
-  data.psd <- data %*% data.psd                       #dim : c x g
-  data.psd <- abs(t(                                  #compute discrete fourier transform
-                    mvfft(t(abs(data.psd)))                         #of each cell sample
-                   )
-                 ) / dim(data)[2]
-  data.psd <- data.psd / rowSums(data.psd)            #divide rowSums from each element
+  data <- t(data)                           #dim : c x g
+  data.psd <- cor(data)                     #dim : g x g
+  data.psd <- data %*% data.psd             #dim : c x g
+  data.psd <- abs(mvfft(t(abs(data.psd)))   #compute discrete fourier transform of each cell sample
+                 ) / dim(data)[2]           #dim : g x c
+  data.psd <- apply(data.psd, 2,            #divide column sum from each element
+                    function(x){x / sum(x)}
+                    )
   data.psd <- data.psd * log2(data.psd)
-  data.psd <- apply(data.psd, 2,
+  data.psd <- apply(data.psd, 1,            #subtract row mean from each element
                     function(x){return (x - mean(x))}
-                    )                                 #subtract column mean from each element
+                    )
   data.psd <- apply(data.psd, 2,
                     function(x){
                       return ( (x-min(x)) / (max(x)-min(x)) )
                     })
+  return (t(data.psd))
+}
+
+#' @export
+psd_without_t <- function(data){
+  data <- t(data)                           #dim : c x g
+  data.psd <- cor(data)                     #dim : g x g
+  data.psd <- data %*% data.psd             #dim : c x g
+  data.psd <- abs(mvfft(t(abs(data.psd)))   #compute discrete fourier transform of each cell sample
+  ) / dim(data)[2]           #dim : g x c
+  data.psd <- apply(data.psd, 2,            #divide column sum from each element
+                    function(x){x / sum(x)}
+  )
+  data.psd <- data.psd * log2(data.psd)
+  data.psd <- sweep(data.psd, 1, rowMeans(data.psd))
+  row_max <- apply(data.psd, 1, max)
+  row_min <- apply(data.psd, 1, min)
+  data.psd <- sweep(
+    sweep(data.psd, 1, row_min),
+    1, row_max - row_min, FUN="/"
+  )
   return (data.psd)
 }
+
+# data <- matrix(c(-1.2225888, 1.34042501, 0.4259692,-0.23716855,
+#                  1.7332330, 0.21851021, -1.0023597, 1.00827522,
+#                  -0.6907052, 0.06109564),
+#                nrow=2, ncol=5, byrow = TRUE)
+# psd(data)
+# psd_without_t(data)
--- test.R
+++ test.R
@@ -6,7 +6,7 @@ devtools::install()
 
 
 library(SingleCellExperiment)
-Sys.setenv(RETICULATE_PYTHON = ".py_env/bin/python")
+Sys.setenv(RETICULATE_PYTHON = "../.py_venv/bin/python")
 library(reticulate)
 source_python('psd.py')
 
@@ -18,15 +18,22 @@ compare_psd_implementation <- function(data, show_result = FALSE){
   psd_r_data <- psdR::psd(data)
   end <- Sys.time()
   print(paste('Using R, psd execution time :', difftime(end, start, units = 'secs')))
+
+  start <- Sys.time()
+  psd_r_data <- psdR::psd_without_t(data)
+  end <- Sys.time()
+  print(paste('Using R sweep, psd execution time :', difftime(end, start, units = 'secs')))
+
   if (show_result) {
     print('psd output :')
     print(psd_r_data)
   }
 
   start <- Sys.time()
-  psd_py_data <- psd(data)
+  psd_py_data <- psd(t(data))
   end <- Sys.time()
   print(paste('Using python via reticulate, psd execution time :', difftime(end, start, units = 'secs')))
+  psd_py_data <- t(psd_py_data)
   if (show_result) {
     print('psd output :')
     print(psd_py_data)
@@ -35,36 +42,38 @@ compare_psd_implementation <- function(data, show_result = FALSE){
   print(all.equal(psd_r_data, psd_py_data))
 }
 
-data1 <- matrix(rnorm(10), nrow=5, ncol=2)
+data1 <- matrix(rnorm(10), nrow=2, ncol=5)
 compare_psd_implementation(data1)
-# [1] "Using R, psd execution time : 0.000609159469604492"
-# [1] "Using python via reticulate, psd execution time : 0.00504446029663086"
+# [1] "Using R, psd execution time : 0.00162935256958008"
+# [1] "Using python via reticulate, psd execution time : 0.00282073020935059"
 # [1] TRUE
 
 data2 <- matrix(rnorm(9), nrow=3, ncol=3)
 compare_psd_implementation(data2)
-# [1] "Using R, psd execution time : 0.000649929046630859"
-# [1] "Using python via reticulate, psd execution time : 0.00477814674377441"
+# [1] "Using R, psd execution time : 0.00117683410644531"
+# [1] "Using python via reticulate, psd execution time : 0.0077660083770752"
 # [1] TRUE
 
 data <- matrix(c(-1.2225888, 1.34042501, 0.4259692,-0.23716855,
                   1.7332330, 0.21851021, -1.0023597, 1.00827522,
                   -0.6907052, 0.06109564),
-                nrow=5, ncol=2, byrow = TRUE)
+                nrow=2, ncol=5, byrow = TRUE)
 compare_psd_implementation(data)
-# [1] "Using R, psd execution time : 0.000904321670532227"
-# [1] "Using python via reticulate, psd execution time : 0.00530862808227539"
+# [1] "Using R, psd execution time : 0.00132465362548828"
+# [1] "Using python via reticulate, psd execution time : 0.00522065162658691"
 # [1] TRUE
 
 
 data <- readRDS(file = 'TabulaMuris_Heart_10X.rds')
 x <- counts(data)
 x <- x[rowSums(x) > 0, ]
-x <- t(as.matrix(x))
+x <- as.matrix(x)
 compare_psd_implementation(x)
-# [1] "Using R, psd execution time : 213.227695226669"
-# [1] "Using python via reticulate, psd execution time : 192.042806386948"
+# [1] "Using R, psd execution time : 210.639530658722"
+# [1] "Using R sweep, psd execution time : 212.108340740204"
+# [1] "Using python via reticulate, psd execution time : 192.281036138535"
 # [1] TRUE
+
 
 
 ##########################################
